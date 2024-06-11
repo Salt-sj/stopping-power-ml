372b41eff28f86307f1c87a1448466ed9d797a0e
Yifan <yifany6@illinois.edu>
2024-06-11 17:03:36 -0500

    mute the output

--- stopping_power_ml/integrator.py
+++ stopping_power_ml/integrator.py
@@ -12,7 +12,6 @@ import numpy as np
 
 class TrajectoryIntegrator:
     """Tool used to compute the stopping power along a certain trajectory"""
-
     def __init__(self, atoms, model, featurizers):
         """Create the class
 
@@ -44,8 +43,8 @@ class TrajectoryIntegrator:
         :return: ndarray, 3x1 array defining the shortest path the covers an entire path"""
 
         # Map the conventional cell lattice vector to the primitive cell
-        prim_vector = np.dot(self.conv_to_prim, np.array(lattice_vector, dtype=np.int))
-        prim_vector = np.array(np.round(prim_vector), dtype=np.int)
+        prim_vector = np.dot(self.conv_to_prim, np.array(lattice_vector, dtype=int))
+        prim_vector = np.array(np.round(prim_vector), dtype=int)
 
         # Determine the shortest-possible vector
         g = gcd(prim_vector[0], gcd(prim_vector[1], prim_vector[2]))
@@ -73,7 +72,7 @@ class TrajectoryIntegrator:
         start_point = np.dot(self.conv_strc.lattice.matrix, start_point)
 
         # Compute the velocity vector
-        velocity_vec = velocity * np.array(traj_vec, np.float) / np.linalg.norm(traj_vec)
+        velocity_vec = velocity * np.array(traj_vec, float) / np.linalg.norm(traj_vec)
 
         # Create the function
         def output(x):
@@ -203,7 +202,7 @@ class TrajectoryIntegrator:
         def output(x):
             # Evaluate the model
             inputs = generator(x)
-            res = self.model.predict(np.array([inputs]))
+            res = self.model.predict(np.array([inputs]), verbose = 0)
             return res if res.shape == () else res[0]
         return output
 
@@ -253,7 +252,7 @@ class TrajectoryIntegrator:
         def output(disp, vel_mag):
             pos = start_point + disp * traj_dir
             x = self.featurizers.featurize(pos, vel_mag * traj_dir)
-            return self.model.predict(np.array([x]))[0]
+            return self.model.predict(np.array([x]), verbose = 0)[0]
         return output
         
 
