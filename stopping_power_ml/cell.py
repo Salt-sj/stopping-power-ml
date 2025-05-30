8d5193117ccddc95371f7af98f1c54ec52e0bd20
Yifan <yifany6@illinois.edu>
2025-05-30 08:30:40 -0500

    conversion between coordinate

--- stopping_power_ml/cell.py
+++ stopping_power_ml/cell.py
@@ -24,18 +24,21 @@ class Cell:
         self.prim_strc = spg.find_primitive()
         self.conv_strc = spg.get_conventional_standard_structure()
 
-        self.input_to_conv = np.round(linalg.solve(self.conv_strc.lattice.matrix, self.simulation_cell.lattice.matrix))
+        self.simulation_to_conv = np.round(linalg.solve(self.conv_strc.lattice.matrix, self.simulation_cell.lattice.matrix))
 
         # Compute the matrix that we will use to map lattice vectors in the conventional cell to ones in the primitive
         self.conv_to_prim = np.round(linalg.solve(self.prim_strc.lattice.matrix, self.conv_strc.lattice.matrix))
 
-        logging.info(f"Original simulation cell (bohr)\n {self.simulation_cell}")
+        logging.info(f"Simulation cell (bohr)\n {self.simulation_cell}")
+        logging.info(f"Simulation cell lattice vectors\n {self.simulation_cell.lattice.matrix}")
         logging.info(f"Conventional unit cell (bohr)\n {self.conv_strc}")
+        logging.info(f"Conventional cell lattice vectors\n {self.conv_strc.lattice.matrix}")
         logging.info(f"Primitive cell (bohr)\n {self.prim_strc}")
-        logging.info(f"Supercell to conventional unit cell conversion\n {self.input_to_conv}")
+        logging.info(f"Primitive cell lattice vectors\n {self.prim_strc.lattice.matrix}")
+        logging.info(f"Simulation cell to conventional unit cell conversion\n {self.simulation_to_conv}")
         logging.info(f"Conventional unit cell to primitive cell conversion\n {self.conv_to_prim}")
 
-    def input_cell_lattice(self):
+    def simulation_cell_lattice(self):
         return self.simulation_cell.lattice.matrix
     
     def conventional_cell_lattice(self):
@@ -43,3 +46,22 @@ class Cell:
 
     def primitive_cell_lattice(self):
         return self.prim_strc.lattice.matrix
+
+    def cartesian_to_conventional(self, pos):
+        return self.conv_strc.lattice.get_fractional_coords(pos)
+
+    def conventional_to_cartesian(self, pos):
+        return self.conv_strc.lattice.get_cartesian_coords(pos)
+
+    def simulation_to_cartesian(self, pos):
+        return self.simulation_cell.lattice.get_cartesian_coords(pos)
+
+    def cartesian_to_simulation(self, pos):
+        return self.simulation_cell.lattice.get_fractional_coords(pos)
+
+    def primitive_to_cartesian(self, pos):
+        return self.prim_strc.lattice.get_cartesian_coords(pos)
+
+    def cartesian_to_primitive(self, pos):
+        return self.prim_strc.lattice.get_fractional_coords(pos)
+
